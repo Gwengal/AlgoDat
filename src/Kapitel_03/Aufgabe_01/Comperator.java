fc647f1e224a90f33fa2886f2648fecf9c0c6450
Gwengal <alexanderkaiser93@yahoo.com>
2024-05-23 20:16:48 +0200

    Kapitel01_Aufgabe01

---
+++ src/Kapitel_03/Aufgabe_01/Comperator.java
@@ -0,0 +1,5 @@
+package Kapitel_03.Aufgabe_01;
+
+public interface Comperator<E, T> {
+    public boolean compare(E key, T element);
+}
---
+++ src/Kapitel_03/Aufgabe_01/Test.java
@@ -0,0 +1,8 @@
+package Kapitel_03.Aufgabe_01;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+}
